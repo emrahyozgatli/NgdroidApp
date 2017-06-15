abb4d339cfc20e2fc41bb3865fac8853ca1ce7cd
emrahyozgatli <emrahyozgatli@hotmail.com>
2017-06-15 16:38:30 +0300

    emrahyozgatli

--- app/src/main/java/com/ngdroidapp/GameCanvas.java
+++ app/src/main/java/com/ngdroidapp/GameCanvas.java
@@ -4,7 +4,10 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 
+import java.util.Vector;
+
 import istanbul.gamelab.ngdroid.base.BaseCanvas;
+import istanbul.gamelab.ngdroid.util.Log;
 import istanbul.gamelab.ngdroid.util.Utils;
 
 
@@ -16,14 +19,15 @@ import istanbul.gamelab.ngdroid.util.Utils;
 
 public class GameCanvas extends BaseCanvas {
 
-    private Bitmap tileset, spritesheet;
+    private Bitmap tileset, spritesheet,bullet;
     private int kareno, animasyonno, animasyonyonu;
-    private int spritex, spritey, hiz, hizx, hizy;
-    private Rect tilesrc, tiledst, spritesrc,spritedst;
+    private int spritex, spritey, hiz, hizx, hizy, bulletoffsetx_temp, bulletoffsety_temp,/*bulletspeedx,bulletspeedy,*/bulletspeed, bulletx_temp, bullety_temp;
+    private Rect tilesrc, tiledst, spritesrc,spritedst,bulletsrc;
+    public Vector<Rect>bulletdst2;
+    public Vector<Integer> bulletx2,bullety2,bulletoffsetx2,bulletoffsety2,bulletspeedx2,bulletspeedy2;
+    int f;
 
     int touchx, touchy;
-
-
 
     public GameCanvas(NgApp ngApp) {
         super(ngApp);
@@ -31,7 +35,7 @@ public class GameCanvas extends BaseCanvas {
 
     public void setup() {
 
-        tileset = Utils.loadImage(root, "images/tilea2.png");
+        tileset = Utils.loadImage(root, "images/tilea2.png"); //TODO sonra bak
         tilesrc = new Rect();
         tiledst = new Rect();
 
@@ -39,37 +43,76 @@ public class GameCanvas extends BaseCanvas {
         spritesrc = new Rect();
         spritedst = new Rect();
 
+        bullet=Utils.loadImage(root,"images/bullet.png");
+        bulletsrc=new Rect();
+
+
+        bulletdst2=new Vector<>();
+        bulletx2 =new Vector<>();
+        bullety2=new Vector<>();
+        bulletoffsetx2=new Vector<>();
+        bulletoffsety2=new Vector<>();
+        bulletspeedx2=new Vector<>();
+        bulletspeedy2=new Vector<>();
+
         kareno = 0;
         spritex=0;
         spritey=0;
         hizx = 0;
-        animasyonno=1;
+        animasyonno=0;
         hizy=0;
         animasyonyonu =0;
         hiz=16;
 
-    }
-
-
-
-    public void update() {
-
-
-
-
-
+        bulletspeed=0;
+        bulletoffsetx_temp =256;
+        bulletoffsety_temp =128;
+        bulletx_temp =0;
+        bullety_temp =0;
     }
 
     public void draw(Canvas canvas) {
-        tilesrc.set(0,0,64,64);
+
         for (int i=0; i<getWidth();i+=128){
             for (int j=0; j<getHeight();j+=128){
                 tiledst.set(i,j,i+128,j+128);
                 canvas.drawBitmap(tileset, tilesrc,tiledst,null);
             }
         }
+        canvas.drawBitmap(spritesheet, spritesrc, spritedst, null);
+
+        for (int i=0;i<bulletx2.size();i++)
+        {
+            canvas.drawBitmap(bullet,bulletsrc,bulletdst2.elementAt(i),null);
+        }
+        // canvas.drawBitmap(bullet,bulletsrc,bulletdst,null);
+    }
+
+    public void update() {
+        tilesrc.set(0,0,64,64);
         spritex += hizx;
         spritey += hizy;
+
+        for (int  i=0;i<bulletx2.size();i++)
+        {
+            bulletx2.set(i,bulletx2.elementAt(i)+bulletspeedx2.elementAt(i));
+            bullety2.set(i,bullety2.elementAt(i)+bulletspeedy2.elementAt(i));
+            if (bulletx2.elementAt(i)>getWidth() || bulletx2.elementAt(i)<0 || bullety2.elementAt(i)>getHeight() || bullety2.elementAt(i)<0 )
+            {
+                bulletx2.removeElementAt(i);
+                bullety2.removeElementAt(i);
+               /* bulletoffsetx2.removeElementAt(i);
+                bulletoffsety2.removeElementAt(i);*/
+                bulletspeedx2.removeElementAt(i);
+                bulletspeedy2.removeElementAt(i);
+                bulletdst2.removeElementAt(i);
+            }
+            Log.i("Control",String.valueOf(bulletx2.size()));
+        }
+
+        /*bulletx_temp+=bulletspeedx;
+        bullety_temp+=bulletspeedy;*/
+
         if(spritex+256>getWidth()){
             spritex= getWidth()-256;
             animasyonno=0;
@@ -104,9 +147,12 @@ public class GameCanvas extends BaseCanvas {
         }
         spritesrc.set(kareno*128,animasyonyonu*128,(kareno+1)*128,(animasyonyonu+1)*128);
         spritedst.set(spritex, spritey, spritex+256, spritey+256);
-        canvas.drawBitmap(spritesheet, spritesrc, spritedst, null);
-
-
+        bulletsrc.set(0,0,70,70);
+        for (int i=0;i<bulletdst2.size();i++)
+        {
+            bulletdst2.elementAt(i).set(bulletx2.elementAt(i),bullety2.elementAt(i),bulletx2.elementAt(i)+32,bullety2.elementAt(i)+32);
+        }
+        //  bulletdst.set(bulletx_temp,bullety_temp,bulletx_temp+32,bullety_temp+32);
     }
 
     public void keyPressed(int key) {
@@ -144,19 +190,19 @@ public class GameCanvas extends BaseCanvas {
     public void touchUp(int x, int y) {
         if (x-touchx>100){
             animasyonno=1;
-            animasyonyonu=1;
+            animasyonyonu=0;
             hizx=hiz;
             hizy=0;
         }
         else if (touchx-x > 100){
             animasyonno=1;
-            animasyonyonu=7;
+            animasyonyonu=1;
             hizx=-hiz;
             hizy=0;
         }
         else if (y-touchy>100){
             animasyonno=1;
-            animasyonyonu=5;
+            animasyonyonu=9;
             hizy=hiz;
             hizx=0;
         }
@@ -170,6 +216,45 @@ public class GameCanvas extends BaseCanvas {
             animasyonno=0;
             hizx=0;
             hizy=0;
+            bulletspeed=32;
+
+            if(animasyonyonu==0)
+            {
+                /*bulletspeedx=bulletspeed;
+                bulletspeedy=0;
+                bulletoffsetx_temp=256;
+                bulletoffsety_temp=128;*/
+                bulletspeedx2.add(bulletspeed);
+                bulletspeedy2.add(0);
+                bulletoffsetx_temp =256;
+                bulletoffsety_temp =128;
+            }
+            else if(animasyonyonu==1)
+            {
+                bulletspeedx2.add(-bulletspeed);
+                bulletspeedy2.add(0);
+                bulletoffsetx_temp =0;
+                bulletoffsety_temp =128;
+            }
+            else if(animasyonyonu==9)
+            {
+                bulletspeedy2.add(bulletspeed);
+                bulletspeedx2.add(0);
+                bulletoffsetx_temp =128;
+                bulletoffsety_temp =256;
+            }
+            else if(animasyonyonu==5)
+            {
+                bulletspeedy2.add(-bulletspeed);
+                bulletspeedx2.add(0);
+                bulletoffsetx_temp =128;
+                bulletoffsety_temp =0;
+            }
+            bulletx2.add(spritex+ bulletoffsetx_temp);
+            bullety2.add(spritey+ bulletoffsety_temp);
+            bulletx_temp =spritex+ bulletoffsetx_temp;
+            bullety_temp =spritey+ bulletoffsety_temp;
+            bulletdst2.add(new Rect(bulletx_temp, bullety_temp, bulletx_temp +32, bullety_temp +32));
         }
 
     }
